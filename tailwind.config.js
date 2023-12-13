a32d48413c6a06446e62f3eab79045fc4d4115ad
Hania <hania.imtisal234@gmail.com>
2023-12-13 21:25:59 +0500

    Define website design and color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "360px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+      },
+      colors: {
+        "sea-salt": "#F5F5F5",
+        "light-blue": "#ADD8E6",
+        "yale-blue": "#00356B",
+        "carolina-blue": "#7BAFD4",
+        "prussian-blue": "#003153",
+      },
+    },
   },
   plugins: [],
 };
