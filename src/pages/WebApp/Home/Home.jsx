608538a266a7d0acae40cd0b3f4ce9d173c2c5c5
Nawal Maqsood <nawalmaqsood16@gmail.com>
2023-12-24 17:55:17 +0500

    Created Landing Page

--- src/pages/WebApp/Home/Home.jsx
+++ src/pages/WebApp/Home/Home.jsx
@@ -2,74 +2,65 @@ import React, { useState } from "react";
 import { Layout, Button, Input } from "antd";
 import AppHeader from "../../../components/Shared/AppHeader/AppHeader.jsx";
 import AppFooter from "../../../components/Shared/AppFooter/AppFooter.jsx";
-import Icon from "../../../assets/Icon.png";
 import { ReadOutlined, QuestionCircleOutlined, InfoCircleOutlined } from "@ant-design/icons";
 
 const Home = () => {
   const [city, setCity] = useState("");
   const [therapistName, setTherapistName] = useState("");
+
   const handleBooking = () => {
-    console.log(`therapis book page ${city} for therapist name: ${therapistName}`);
+    console.log(`therapis ${city} for therapist: ${therapistName}`);
   };
 
   return (
-    <Layout className="mainLayout bg-sea-salt">
+    <Layout className="mainLayout bg-white">
       <AppHeader />
-      <div style={{ textAlign: "center", padding: "20px", zIndex: 1 }}>
-        <h1 style={{ fontSize: "3.5rem", fontWeight: "bold", fontFamily: "'Helvetica Neue', sans-serif", marginBottom: "0.5rem",
-          textShadow: "3px 3px 5px #003366, 5px 5px 7px #56A0D3", color: "#003366"}}>
-          Our-Blues
-        </h1>
-        <p style={{ fontFamily: "'Helvetica Neue'", color: "#003366", fontSize: "1rem" , fontWeight: "bold"}}>
-          Within Our-Blues, we illuminate the path to mental wellness, offering a safe harbor for self-discovery, healing, and the empowering embrace of your unique journey towards inner balance.
-          Our platform is designed to offer expert guidance from certified therapists, fostering a holistic approach that addresses the emotional, mental, and physical dimensions of well-being.
-          Join us on this transformative journey, where Our-Blues becomes a beacon of support, understanding, and empowerment for all.
+      <div className="text-center p-20 z-1 bg-white">
+  <h1 className="text-5xl font-bold font-sans mb-1">
+    <span className="text-shadow-lg text-yale-blue">Our Blues</span></h1>
+        <p className="font-sans text-bold text-lg text-carolina-blue">
+          Within Our-Blues, we illuminate the path to mental wellness, offering a safe harbor for self-discovery, healing, and the empowering embrace of your unique journey towards inner balance. Our platform is designed to offer expert guidance from certified therapists, fostering a holistic approach that addresses the emotional, mental, and physical dimensions of well-being. Join us on this transformative journey, where Our-Blues becomes a beacon of support, understanding, and empowerment for all.
         </p>
       </div>
-
-      <div className="container" style={{ background: "linear-gradient(to right, #56A0D3, #003366)", padding: "20px", marginLeft: "10px", textAlign: "center", color: "#FFF", borderRadius: "10px", position: "relative", zIndex: 1, marginTop:"30px"}}>
-        <h2 style={{ fontSize: "2rem", fontWeight: "bold", marginBottom: "1rem", fontFamily: "'Helvetica Neue'" }}>
+      <div className="container bg-yale-blue p-10  text-center text-white rounded-10 relative z-1 ">
+        <h2 className="text-2xl font-bold mb-1 font-sans text-white">
           Find and Book Your Therapist Now
         </h2>
-        <div className="booking-form" style={{ marginLeft: "10px" }}>
+        <div className="booking-form md:ml-0 ml-10">
           <Input
             placeholder="Enter your city"
             value={city}
             onChange={(cityInput) => setCity(cityInput.target.value)}
+            className="ant-input bg-carolina-blue text-white"
           />
           <Input
             placeholder="Enter therapist's name"
             value={therapistName}
             onChange={(therapistInput) => setTherapistName(therapistInput.target.value)}
+            className="ant-input bg-carolina-blue text-white mt-3"
           />
-          <Button type="primary" size="large" onClick={handleBooking} style={{ backgroundColor: "#003366", color: "#56A0D3", marginTop: "10px" }}>
+          <Button type="primary" size="large" onClick={handleBooking} className="bg-carolina-blue text-white mt-5">
             Book Therapist
           </Button>
         </div>
       </div>
 
-      <div
-        className="flex items-center justify-center m-5 bg-top blur-background"
-        style={{backgroundImage: `url(${Icon})`,backgroundSize: "cover",minHeight: "50vh",position: "fixed",width: "210vh",height: "80vh",filter: "blur(40px)", zIndex: 0,}}></div>
-      <div className="container" style={{ background: "linear-gradient(to right, white, #56A0D3)", padding: "10px", marginLeft: "20px", textAlign: "left", color: "#003366", borderRadius: "10px", position: "relative", zIndex: 1, marginTop: "50px", width: "300px" }}>
-        <h2 style={{ fontSize: "1.8rem", fontWeight: "bold", marginBottom: "0.8rem", fontFamily: "'sans-serif'", color: "#003366" }}>
-          Looking for more?
-        </h2>
-        <p style={{ fontSize: "1rem", color: "#003366", marginBottom: "0.5rem" }}>
-          Explore our additional resources related to our app:
-        </p>
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", marginTop: "5px" }}>
-          <Button type="link" style={{ color: "#003366", marginBottom: "5px", textAlign: "left" }} icon={<ReadOutlined />}>
-            Read Mental Health Articles
-          </Button>
-          <Button type="link" style={{ color: "#003366", marginBottom: "5px", textAlign: "left" }} icon={<QuestionCircleOutlined />}>
-            Ask a Question
-          </Button>
-          <Button type="link" style={{ color: "#003366", textAlign: "left" }} icon={<InfoCircleOutlined />}>
-            About Us
-          </Button>
-        </div>
-      </div>
+      <div className="container bg-white bg-gradient-to-r from-yale-blue to-carolina-blue text-left text-white rounded-10 relative z-1 mt-20 shadow-lg ">
+  <h2 className="text-2xl font-bold mb-4 font-sans text-center mt-10">
+    Looking for more?
+  </h2>
+  <div className="flex items-center justify-around mb-10 text-font-sans">
+    <Button type="link" className="text-white text-lg " icon={<ReadOutlined />}>
+      Read Mental Health Articles
+    </Button>
+    <Button type="link" className="text-white text-lg" icon={<QuestionCircleOutlined />}>
+      Ask a Question
+    </Button>
+    <Button type="link" className="text-white text-lg" icon={<InfoCircleOutlined />}>
+      About Us
+    </Button>
+  </div>
+</div>
 
       <Layout style={{ marginTop: "100vh" }}>
         <AppFooter />
